3a47ead6dd7244791a8a9ed25e5ccb917ce14261
Patrick <trybikpolski@gmail.com>
2022-05-31 16:27:05 +0200

    Created migration to added expire_at field to tickets table

---
+++ database/migrations/2022_05_31_141751_add_expire_at_to_tickets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->timestamp('expire_at')->nullable()->default(Carbon::parse(now())->addHours(24)->format('Y-m-d H:i:s'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            //
+        });
+    }
+};
