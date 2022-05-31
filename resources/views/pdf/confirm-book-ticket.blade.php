255df1c811a66cda59318d5eae158d87053d9602
Patrick <trybikpolski@gmail.com>
2022-05-31 16:01:23 +0200

    Improvement PDF template

--- resources/views/pdf/confirm-book-ticket.blade.php
+++ resources/views/pdf/confirm-book-ticket.blade.php
@@ -10,10 +10,43 @@
     </style>
 </head>
 <body>
-<h1>{{ $event_title }}</h1>
-<p>Wydarzenie rozpoczyna się {{ $event_starts_at }}</p>
-<p>{{ $event_description }}</p>
-<hr />
-Aby anulować rezerwację wejdź na {{ route('bookOffTicketForm', $ticket_number) }}
+    <div class="container">
+        <div class="row mb-3">
+            <div class="col-sm-12 float-end right">
+                <span>Numer rezerwacji {{ $ticket_number }}</span>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <div class="col-sm-12">
+                <h4 class="text-uppercase text-center">{{ $event_title }}</h4>
+                <small>Wydarzenie rozpoczyna się {{ $event_starts_at }}</small>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <div class="col-sm-12">
+                <h4>Opis wydarzenia</h4>
+                <p>
+                    {{ $event_description }}
+                </p>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <div class="col-sm-12">
+                <div class="row mb-3">
+                    <div class="col-sm-12">
+                        Pamiętaj! Jeżeli nie jesteś w stanie pojawić się na wydarzeniu, anuluj rezerwacje. Na Twoje miejsce może czegoś ktoś, komu bardzo zależy na tym wydarzeniu. Bądźmy dla siebie ludźmi :)
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-sm-12">
+                        <a href="{{ route('bookOffTicketForm', $ticket_number) }}" class="btn btn-primary">Anuluj rezerwację</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
