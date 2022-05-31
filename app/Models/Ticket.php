940321bac9bfa7473c30976a1c0b461b66f28eb7
Patrick <trybikpolski@gmail.com>
2022-05-31 14:37:44 +0200

    Created Ticket model

---
+++ app/Models/Ticket.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var     string[]
+     */
+    protected $fillable = [
+        'email', 'name', 'last_name'
+    ];
+
+    /**
+     * @return  string
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'ticket_number';
+    }
+
+    /**
+     * @return  BelongsTo
+     */
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+}
