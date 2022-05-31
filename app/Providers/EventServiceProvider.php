259600c8be9f399db60d04768fa5f7a243ffb850
Patrick <trybikpolski@gmail.com>
2022-05-31 14:38:57 +0200

    Added observers to EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,17 +2,20 @@
 
 namespace App\Providers;
 
+use App\Models\Event;
+use App\Models\Ticket;
+use App\Observers\EventObserver;
+use App\Observers\TicketObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
     /**
      * The event listener mappings for the application.
      *
-     * @var array<class-string, array<int, class-string>>
+     * @var     array<class-string, array<int, class-string>>
      */
     protected $listen = [
         Registered::class => [
@@ -23,10 +26,11 @@ class EventServiceProvider extends ServiceProvider
     /**
      * Register any events for your application.
      *
-     * @return void
+     * @return  void
      */
     public function boot()
     {
-        //
+        Event::observe(EventObserver::class);
+        Ticket::observe(TicketObserver::class);
     }
 }
