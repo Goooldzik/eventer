2bbf61f3da40e34ea4a1a59cd1f1fea96c6e2d6b
Patrick <trybikpolski@gmail.com>
2022-06-02 01:21:14 +0200

    Created view for privacy policy page

---
+++ resources/views/pages/privacy-policy.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row mb-3">
+        <div class="col-sm-12">
+            <div class="row mb-3">
+                <div class="col-sm-12">
+                    <h4 class="text-uppercase text-center">Polityka prywatności</h4>
+                </div>
+            </div>
+
+            <div class="rpw mb-3">
+                <div class="col-sm-12">
+                    A. Wprowadzenie
+                    <ol>
+                        <li>Prywatność odwiedzających naszą stronę internetową jest dla nas bardzo ważna i dokładamy wszelkich starań, aby ją chronić. Niniejsza polityka wyjaśnia, co robimy z Twoimi danymi osobowymi.</li>
+                        <li>Zgoda na korzystanie z plików cookie zgodnie z warunkami niniejszej polityki podczas pierwszej wizyty na naszej stronie pozwala nam na korzystanie z plików cookie przy każdej kolejnej wizycie na naszej stronie.</li>
+                    </ol>
+
+                    B. Zbieranie danych osobowych
+                    <small>Następujące rodzaje danych osobowych mogą być gromadzone, przechowywane i wykorzystywane:</small>
+                    <ol>
+                        <li>informacje o komputerze, w tym adres IP, lokalizacja geograficzna, typ i wersja przeglądarki oraz system operacyjny;</li>
+                        <li>informacje o Twoich wizytach i korzystaniu z tej witryny, w tym źródło odesłań, długość wizyty, wyświetlenia stron i ścieżki nawigacji w witrynie;</li>
+                        <li>informacje, takie jak adres e-mail, które podajesz podczas rejestracji w naszej witrynie internetowej;</li>
+                        <li>informacje wprowadzane podczas tworzenia profilu w naszej witrynie — na przykład imię i nazwisko, zdjęcia profilowe, płeć, data urodzin, status związku, zainteresowania i hobby, dane edukacyjne i dane dotyczące zatrudnienia;</li>
+                        <li>informacje, takie jak imię i nazwisko oraz adres e-mail, które podajesz w celu skonfigurowania subskrypcji naszych e-maili lub biuletynów;</li>
+                        <li>informacje wprowadzane podczas korzystania z usług na naszej stronie internetowej;</li>
+                        <li>informacje, które są generowane podczas korzystania z naszej strony internetowej, w tym kiedy, jak często i w jakich okolicznościach z niej korzystasz;</li>
+                        <li>informacje dotyczące wszystkiego, co kupujesz, usług, z których korzystasz lub transakcji dokonywanych za pośrednictwem naszej strony internetowej, w tym imię i nazwisko, adres, numer telefonu, adres e-mail i dane karty kredytowej;</li>
+                        <li>informacje publikowane na naszej stronie internetowej z zamiarem opublikowania ich w Internecie, w tym nazwa użytkownika, zdjęcia profilowe i treść umieszczanych postów;</li>
+                        <li>informacje zawarte we wszelkiej korespondencji przesyłanej do nas e-mailem lub za pośrednictwem naszej strony internetowej, w tym treści komunikacyjne i metadane;</li>
+                        <li>wszelkie inne dane osobowe, które nam przesyłasz.</li>
+                    </ol>
+                    <small>Zanim ujawnisz nam dane osobowe innej osoby, musisz uzyskać zgodę tej osoby na ujawnienie i przetwarzanie tych danych osobowych zgodnie z niniejszymi zasadami</small>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
