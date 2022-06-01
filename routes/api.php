4c598e942b9d70658fc4b214b34d77c0cb7b7d09
Patrick <trybikpolski@gmail.com>
2022-06-02 00:31:02 +0200

    Improvement routing for API

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/book-off/{ticket}', [TicketService::class, 'bookOffTicket'])->name('api.book-off');
 Route::post('/book/{event}', [TicketService::class, 'bookTicket'])->name('api.book-ticket');
-Route::put('/change-event-date/{event}', [EventService::class, 'changeDate'])->name('api.change-event-date');
+Route::put('/change-event-date/{event}', [EventController::class, 'changeDate'])->name('api.change-event-date');
 
 Route::get('/get-tickets/{event}', [EventController::class, 'getAllTickets'])->name('api.get-tickets');
