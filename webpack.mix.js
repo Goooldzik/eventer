6a5685b7b2b5e05a759a23c21b69850516696452
Patrick <trybikpolski@gmail.com>
2022-06-02 00:30:25 +0200

    Added new JS files to webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,21 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+
+    .js('resources/js/events/changeEventDate.js', 'public/js/events')
+    .js('resources/js/events/deleteEvent.js', 'public/js/events')
+    .js('resources/js/events/createEvent.js', 'public/js/events')
+
+    .js('resources/js/pages/createPage', 'public/js/pages')
+    .js('resources/js/pages/updatePage', 'public/js/pages')
+
+    .js('resources/js/tickets/bookTicket.js', 'public/js/tickets')
+    .js('resources/js/tickets/book-off.js', 'public/js/tickets')
+
+    .js('resources/js/users/createNewUser.js', 'public/js/users')
+    .js('resources/js/users/updateUser.js', 'public/js/users')
+
     .sass('resources/sass/app.scss', 'public/css')
+
+    .copy('node_modules/chart.js/dist/chart.js', 'public/js')
     .sourceMaps();
