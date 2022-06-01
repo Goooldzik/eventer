5a5dbb03a346cf0f7d90a41d327dc6205d4b33bd
Patrick <trybikpolski@gmail.com>
2022-06-01 23:26:52 +0200

    Created API for update event and get tickets for select event

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\EventController;
+use App\Services\API\EventService;
 use App\Services\API\TicketService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/book-off/{ticket}', [TicketService::class, 'bookOffTicket'])->name('api.book-off');
 Route::post('/book/{event}', [TicketService::class, 'bookTicket'])->name('api.book-ticket');
+Route::put('/change-event-date/{event}', [EventService::class, 'changeDate'])->name('api.change-event-date');
+
+Route::get('/get-tickets/{event}', [EventController::class, 'getAllTickets'])->name('api.get-tickets');
